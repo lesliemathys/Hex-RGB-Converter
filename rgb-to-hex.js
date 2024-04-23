aede9089a3161e778e9732991d0ef8514cf23065
Leslie Mathys <dev@lesliemathys.com>
2024-04-23 10:42:37 +1000

    added a hexKey object that will be used to reference in calculations

---
+++ rgb-to-hex.js
@@ -0,0 +1,10 @@
+const hexKey = {
+    0:0, 1:1, 2:2, 3:3,
+    4:4, 5:5, 6:6, 7:7,
+    8:8, 9:9, 10:'A', 11:'B',
+    12:'C', 13:'D', 14:'E', 15:'F'
+};
+
+//event listener to default numbers over 255 to max value of 255
+
+//event listener to default negative numbers to 0
