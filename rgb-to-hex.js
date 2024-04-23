b433c7d2245d065ac6bec047467c2a014bcc7ec4
Leslie Mathys <dev@lesliemathys.com>
2024-04-23 11:07:07 +1000

    changed key to strings

--- rgb-to-hex.js
+++ rgb-to-hex.js
@@ -1,13 +1,15 @@
 const rgbToHexKey = {
-    0:0, 1:1, 2:2, 3:3,
-    4:4, 5:5, 6:6, 7:7,
-    8:8, 9:9, 10:'A', 11:'B',
-    12:'C', 13:'D', 14:'E', 15:'F'
+    '0':'0', '1':'1', '2':'2', '3':'3',
+    '4':'4', '5':'5', '6':'6', '7':'7',
+    '8':'8', '9':'9', '10':'A', '11':'B',
+    '12':'C', '13':'D', '14':'E', '15':'F'
 };
 
 //event listener to default numbers over 255 to max value of 255
 
 //event listener to default negative numbers to 0
+
+//on submit, change input to string
 
 // r, g, b values in > 
 //divide by 16 > 
