f3abe889f8726f566913827c47cef7ab60eb08fb
Leslie Mathys <dev@lesliemathys.com>
2024-04-26 15:22:41 +1000

    Updated comments

--- rgb-to-hex.js
+++ rgb-to-hex.js
@@ -33,12 +33,14 @@ rgbInputs.addEventListener('click', function(){
     }
 );
 
+// divide rgb value by 16 to give how many sixteens. this will be used to query hex key to give first part of each hex code segment (#x_x_x_)
 function calculateSixteens(){
     for (let i =0; i < rgbValues.length; i++) {
         hexSixteens.push(Math.floor(rgbValues[i] / 16));
     }
 }
 
+// calculate how many ones are in the rgb value, this will be used to query hex key to give second part of each hex code segment (#_x_x_x)
 function calculateOnes(){
     for (let i =0; i < rgbValues.length; i++) {
         hexOnes.push(rgbValues[i] - (hexSixteens[i] * 16));
