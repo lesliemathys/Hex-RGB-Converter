461f2ee6baaf2b69c4a4e152b9b3eb61647b5146
Leslie Mathys <dev@lesliemathys.com>
2024-04-26 13:35:59 +1000

    re-worked JS so that we use arrays to store RGB values instead

--- rgb-to-hex.js
+++ rgb-to-hex.js
@@ -1,25 +1,30 @@
-let r = document.getElementById('red-input');
-let g = document.getElementById('green-input');
-let b = document.getElementById('blue-input');
+let redVal = document.getElementById('red-input');
+let greenVal = document.getElementById('green-input');
+let blueVal = document.getElementById('blue-input');
 let hexOutput = document.getElementById('hexOutput')
 
-const form = document.getElementById('rgb-input');
+const rgbValues = [];
+const rgbInputs = document.getElementById('convert-to-hex');
 
-form.addEventListener('submit', function(e){
+rgbInputs.addEventListener('click', function(){
+
+    // check if array is empty. If so add inputs to it afresh
+    if (rgbValues === 0){
+
+        rgbValues.push(redVal.value, greenVal.value, blueVal.value);
+
+    } 
     
-    e.preventDefault();
-
-    r = convertToHex(r.value);
-    g = convertToHex(g.value);
-    b = convertToHex(b.value);
-
-    rgb = r + g + b;
-    rgb = rgb.toUpperCase();
-
-    function convertToHex(rgb) {
-        let hexCode = Number(rgb).toString(16);
-        return hexCode;
+    // if array is not empty, create a new array that replaces the rgbValues array
+    else {
+        const replacementRgbValues = [];
+        replacementRgbValues.push(redVal.value, greenVal.value, blueVal.value);
+        rgbValues.splice(0, 3, ...replacementRgbValues)
+        console.log(rgbValues);
     }
 
-    hexOutput.value = rgb;
-})
+    hexOutput.value = rgbValues[0];
+
+    }
+);
+
