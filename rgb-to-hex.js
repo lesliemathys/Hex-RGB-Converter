c3153e47ab7ae94b0552e66c0c1db452b3a4bee4
Leslie Mathys <dev@lesliemathys.com>
2024-04-26 15:19:09 +1000

    Receive user input, break into sixteens and ones

--- rgb-to-hex.js
+++ rgb-to-hex.js
@@ -4,15 +4,19 @@ let blueVal = document.getElementById('blue-input');
 let hexOutput = document.getElementById('hexOutput')
 
 const rgbValues = [];
+const hexSixteens = [];
+const hexOnes = [];
 const rgbInputs = document.getElementById('convert-to-hex');
 
+// function that grabs user RGB input and then processes it
 rgbInputs.addEventListener('click', function(){
 
     // check if array is empty. If so add inputs to it afresh
     if (rgbValues === 0){
 
         rgbValues.push(redVal.value, greenVal.value, blueVal.value);
-
+        calculateSixteens()
+        calculateOnes()
     } 
     
     // if array is not empty, create a new array that replaces the rgbValues array
@@ -20,7 +24,8 @@ rgbInputs.addEventListener('click', function(){
         const replacementRgbValues = [];
         replacementRgbValues.push(redVal.value, greenVal.value, blueVal.value);
         rgbValues.splice(0, 3, ...replacementRgbValues)
-        console.log(rgbValues);
+        calculateSixteens()
+        calculateOnes()
     }
 
     hexOutput.value = rgbValues[0];
@@ -28,3 +33,14 @@ rgbInputs.addEventListener('click', function(){
     }
 );
 
+function calculateSixteens(){
+    for (let i =0; i < rgbValues.length; i++) {
+        hexSixteens.push(Math.floor(rgbValues[i] / 16));
+    }
+}
+
+function calculateOnes(){
+    for (let i =0; i < rgbValues.length; i++) {
+        hexOnes.push(rgbValues[i] - (hexSixteens[i] * 16));
+    }
+}
