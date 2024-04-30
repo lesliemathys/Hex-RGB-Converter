d35da44dafd566617fbeb7523a94cbc041dfb496
Leslie Mathys <dev@lesliemathys.com>
2024-04-30 21:48:24 +1000

    Code splits hex input into individual characters, and converts from hex into decimal

--- hex-to-rgb.js
+++ hex-to-rgb.js
@@ -1,15 +1,21 @@
 let hexValue = document.getElementById('hex-input');
+let hexCode = [];
 
 const hexInput = document.getElementById('convert-to-rgb')
-const hexCode = [];
-const hexRed = [];
-const hexGreen = [];
-const hexBlue = [];
+const rgbOutput = [];
 
 hexInput.addEventListener('click', function(){
     if (hexCode.length === 0) {
-        hexCode.push(hexValue.value);
+        hexCode = hexValue.value.split("");
+        convertToDecimal()
     }
-    return console.log(hexCode);
+    console.log(hexCode);
+    console.log(rgbOutput);
 
 })
+
+function convertToDecimal(){
+    for (let i = 0; i < hexCode.length; i++){
+        rgbOutput.push(parseInt(hexCode[i], 16));
+    }
+}
