2d63ba19d7ce62106f491f9f84c80e68f9fd7b3a
Leslie Mathys <dev@lesliemathys.com>
2024-04-30 20:55:37 +1000

    Code takes in hex code, and pushes it to an array

--- hex-to-rgb.js
+++ hex-to-rgb.js
@@ -1,9 +1,15 @@
-const hexInputs = document.getElementById('convert-to-rgb');
+let hexValue = document.getElementById('hex-input');
+
+const hexInput = document.getElementById('convert-to-rgb')
 const hexCode = [];
 const hexRed = [];
 const hexGreen = [];
 const hexBlue = [];
 
-hexInputs.addEventListener('click', function(){
-    
+hexInput.addEventListener('click', function(){
+    if (hexCode.length === 0) {
+        hexCode.push(hexValue.value);
+    }
+    return console.log(hexCode);
+
 })
